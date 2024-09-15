2938d75a2ba4d8ed4ef81b175bc94e7bafd6f015
Grigoriy Efimov <efimov-gm@newpage.xyz>
2024-09-15 22:53:52 +0300

    Add randomize init

--- src/main/java/com/catinbeard/artificiallife/Game.java
+++ src/main/java/com/catinbeard/artificiallife/Game.java
@@ -5,6 +5,7 @@
 package com.catinbeard.artificiallife;
 
 import java.io.IOException;
+import java.util.Random;
 
 /**
  *
@@ -14,11 +15,17 @@ public class Game {
 
     protected Visualizer visualizer;
     protected Field field;
+    protected double grassRate;
 
-    public Game() {
-
-        int size = 50;
-
+    public Game(int size){
+        this(size, 0.1);
+    }
+    public Game(){
+        this(20);
+    }
+    
+    public Game(int size, double grass) {
+        grassRate = grass;
         visualizer = new Visualizer();
         field = new Field(size, size);
         field.addObserver(visualizer);
@@ -27,10 +34,11 @@ public class Game {
     }
     
     protected void initField(){
+        Random random = new Random();
         try {
             for (int i = 0; i < field.getSizeX(); i++) {
                 for (int j = 0; j < field.getSizeY(); j++) {
-                    if((i+j) % 2 == 0)
+                    if(random.nextDouble() < grassRate)
                     field.insertObject(i, j, new Grass());
                 }
             }
